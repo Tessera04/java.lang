31764132b642d670e226a5714737b0cd644b8c95
Matias <matute_11@hotmail.com.ar>
2023-02-02 00:05:33 -0300

    Cast

--- src/com/bytebank/test/TestArrayReferencias.java
+++ src/com/bytebank/test/TestArrayReferencias.java
@@ -1,6 +1,9 @@
 package com.bytebank.test;
 
 import com.bytebank.modelo.cuentaCorriente;
+import com.bytebank.modelo.Cuenta;
+import com.bytebank.modelo.cuentaAhorro;
+
 
 public class TestArrayReferencias {
 	
@@ -8,11 +11,21 @@ public class TestArrayReferencias {
 		
 		cuentaCorriente cc = new cuentaCorriente(23, 44);
 		
-		cuentaCorriente[] cuentas = new cuentaCorriente[5];
+		Cuenta[] cuentas = new Cuenta[5];
 		cuentas[1] = cc;
+		
+		cuentaAhorro ca = new cuentaAhorro(11, 23);
+		cuentas[2] = ca;
+		
+		//CAST DE ETIQUETAS
+		cuentaCorriente cuenta = (cuentaCorriente) cuentas[1];
+		System.out.println(cuenta);
 		
 		System.out.println(cc);
 		System.out.println(cuentas[1]);
+		
+		System.out.println(ca);
+		System.out.println(cuentas[2]);
 		
 		cuentas[0] = new cuentaCorriente(11, 99);
 		System.out.println(cuentas[0]);
